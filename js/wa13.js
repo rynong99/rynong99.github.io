90b60d4e049c65b1faf45afa70b03834f4527d7c
rynong99 <ryon5157@colorado.edu>
2024-04-18 12:10:33 -0600

    WA13

---
+++ js/wa13.js
@@ -0,0 +1,69 @@
+const personnel = {
+    'workers':[
+        {'firstName' : 'Sam',
+        'department' : 'Tech',
+        'role' : 'Manager',
+        'salary' : 40000,
+        'raiseEligible' : true
+        },
+        {'firstName': 'Mary',
+        'department' : 'Finance',
+        'role' : 'Trainee',
+        'salary' : 18500,
+        'raiseEligible' : true
+        },
+        {'firstName' : 'Bill',
+        'department' : 'HR',
+        'role' : 'Executive',
+        'salary' : 21200,
+        'raiseEligible' : false
+        }
+    ]
+}
+console.log('Question 1:')
+console.log(personnel)
+
+const company = {
+    companyName : 'Tech Stars',
+    website : 'www.techstars.site',
+    employees : personnel
+}
+console.log('Question 2:') 
+console.log(company)
+
+company.employees.workers.push({'firstName' : 'Anna',
+'department' : 'Tech',
+'role' : 'Executive',
+'salary' : 25600,
+'raiseEligible' : false
+})
+console.log('Question 3:')
+console.log(company.employees.workers)
+
+var totalSalary = 0
+for (let i = 0; i < company.employees.workers.length;i++){
+    totalSalary += company.employees.workers[i].salary;
+}
+console.log('Question 4:')
+console.log(totalSalary)
+function raise(){
+    for (let i = 0; i < company.employees.workers.length;i++){
+        if (company.employees.workers[i].raiseEligible == true){
+            company.employees.workers[i].salary *= 1.1;
+            company.employees.workers[i].raiseEligible = false;
+        }
+    }
+}
+raise()
+console.log('Question 5:')
+console.log(company.employees.workers)
+
+const homeWorkers= ['Anna','Sam']
+for (let i = 0; i < company.employees.workers.length;i++){
+    company.employees.workers[i].wfh = false
+    if (homeWorkers.includes(company.employees.workers[i].firstName)){
+        company.employees.workers[i].wfh = true
+    }
+}
+console.log('Question 6:')
+console.log(company.employees.workers)
